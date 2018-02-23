6fe6e7c02f2e54e7bb8d76d190715a5a94ef402c
alexjondiaz <alexjondiaz@gmail.com>
2018-02-23 13:33:19 +0100

    changed to arrow operator

--- display_ui.c
+++ display_ui.c
@@ -31,40 +31,40 @@ void draw(Object o) {
 void move(Object* o, int dir) {
     int i, j;
 
-    for (i = 0; i <= (*o).size / 2; i++)
-        for (j = 0; j <= (*o).size / 2; j++)
-            unset_pixel(i + (*o).posX, j + (*o).posY);
+    for (i = 0; i <= o->size / 2; i++)
+        for (j = 0; j <= o->size / 2; j++)
+            unset_pixel(i + o->posX, j + o->posY);
 
     switch (dir) {
         case LEFT:
-            if ((*o).posY + (*o).size < WIDTH + 3) (*o).posY += (*o).velocity;
+            if (o->posY + o->size < WIDTH + 3) o->posY += o->velocity;
             break;
         case RIGHT:
-            if ((*o).posY > 1) (*o).posY -= (*o).velocity;
+            if (o->posY > 1) o->posY -= o->velocity;
             break;
         case UP:
-            if ((*o).posX + (*o).size < HEIGHT + 4) (*o).posX += (*o).velocity;
+            if (o->posX + o->size < HEIGHT + 4) o->posX += o->velocity;
             break;
         case DOWN:
-            if ((*o).posX > 10) (*o).posX -= (*o).velocity;
+            if (o->posX > 10) o->posX -= o->velocity;
             break;
     }
 
-    for (i = (*o).posX; i < (*o).size; i++)
-        for (j = (*o).posY; j < (*o).size; j++) set_pixel(i, j);
+    for (i = o->posX; i < o->size; i++)
+        for (j = o->posY; j < o->size; j++) set_pixel(i, j);
 }
 
 void drop(Rock* r) {
     int i, j;
 
-    for (i = 0; i <= (*r).size / 2; i++)
-        for (j = 0; j <= (*r).size / 2; j++)
-            unset_pixel(i + (*r).posX, j + (*r).posY);
+    for (i = 0; i <= r-> size / 2; i++)
+        for (j = 0; j <= r->size / 2; j++)
+            unset_pixel(i + r->posX, j + r->posY);
 
-    (*r).posX -= (*r).velocity;
+    r->posX -= r->velocity;
 
-    for (i = (*r).posX; i < (*r).size; i++)
-        for (j = (*r).posY; j < (*r).size; j++) set_pixel(i, j);
+    for (i = r->posX; i < r->size; i++)
+        for (j = r->posY; j < r->size; j++) set_pixel(i, j);
 
     draw(*r);
 }
